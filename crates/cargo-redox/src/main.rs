82c799742dc66ca6298759239d8f83e87fcf6375
gibbz00 <gabrielhansson00@gmail.com>
2023-11-05 14:10:52 +0100

    podman: Init subcommand.

--- crates/cargo-redox/src/main.rs
+++ crates/cargo-redox/src/main.rs
@@ -1,1 +1,17 @@
-fn main() {}
+use cargo_redox_podman::PodmanArgs;
+use clap::Parser;
+
+#[derive(clap::Parser)]
+struct Args {
+    #[command(subcommand)]
+    sub_command: SubCommands,
+}
+
+#[derive(clap::Subcommand)]
+enum SubCommands {
+    Podman(PodmanArgs),
+}
+
+fn main() {
+    let _args = Args::parse();
+}
